58531d1e5cb1b6dab780145f370708209e649abf
manishmamidipally <mamidipallymanish6@gmail.com>
2026-09-13 11:52:58 +0530

    Added The Exception Layer

---
+++ src/com/eventbooking/exception/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package com.eventbooking.exception;
+
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
